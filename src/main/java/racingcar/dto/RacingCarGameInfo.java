cff691cd33ad4310e846e23f23a9b7b0a8598b39
lhoju0158 <lhoju0158@gmail.com>
2025-10-26 18:31:44 +0900

    feat(dto): 데이터 전달 객체 구현

--- src/main/java/racingcar/dto/RacingCarGameInfo.java
+++ src/main/java/racingcar/dto/RacingCarGameInfo.java
@@ -1,4 +1,23 @@
 package racingcar.dto;
 
 public class RacingCarGameInfo {
+    private int gameCount;
+    private String carNames;
+
+    private RacingCarGameInfo(String carNames, int gameCount) {
+        this.gameCount = gameCount;
+        this.carNames = carNames;
+    }
+
+    public static RacingCarGameInfo of(String carNames, int gameCount) {
+        return new RacingCarGameInfo(carNames, gameCount);
+    }
+
+    public int getGameCount() {
+        return gameCount;
+    }
+
+    public String getCarNames() {
+        return carNames;
+    }
 }
--- src/main/java/racingcar/dto/RacingCarGameResult.java
+++ src/main/java/racingcar/dto/RacingCarGameResult.java
@@ -1,14 +1,16 @@
 package racingcar.dto;
 
-import java.util.List;
-
-public class RacingCarGameResultDto {
+public class RacingCarGameResult {
     private String gameResult;
     private String winners;
 
-    public RacingCarGameResultDto(String gameResult, String winners) {
+    private RacingCarGameResult(String gameResult, String winners) {
         this.gameResult = gameResult;
         this.winners = winners;
+    }
+
+    public static RacingCarGameResult of(String gameResult, String winners) {
+        return new RacingCarGameResult(gameResult, winners);
     }
 
     public String getGameResult() {
