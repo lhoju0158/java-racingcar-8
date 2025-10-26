854985fcf435cb734a34d2c4ee0655c6e7be895b
lhoju0158 <lhoju0158@gmail.com>
2025-10-26 18:29:16 +0900

    feat(error): 에러 메세지 작성

--- src/main/java/racingcar/error/ErrorMessage.java
+++ src/main/java/racingcar/error/ErrorMessage.java
@@ -1,4 +1,23 @@
 package racingcar.error;
 
 public enum ErrorMessage {
+    OUTRAGE_RACING_CAR_COUNT("자동차의 개수는 %d 이하 양수여야합니다."),
+    INVALID_RACING_CAR_NAME("자동차의 이름 글자 수는 %d 이하 양수여야합니다."),
+    INVALID_GAME_COUNT_VALUE("게임 횟수는 %d 이하 양수입니다."),
+    INVALID_GAME_COUNT_TYPE("게임 횟수는 정수입니다.");
+
+    private String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getMessage(int max) {
+        return String.format(message, max);
+    }
+
 }
