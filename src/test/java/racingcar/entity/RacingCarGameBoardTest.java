ba82e84168a1ebeccc73166ceef9b34369bf3982
lhoju0158 <lhoju0158@gmail.com>
2025-10-26 18:28:26 +0900

    test(entity): 입력 검증 테스트, 기록 검증 테스트 구현

--- src/test/java/racingcar/entity/RacingCarGameBoardTest.java
+++ src/test/java/racingcar/entity/RacingCarGameBoardTest.java
@@ -1,4 +1,79 @@
 package racingcar.entity;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.error.ErrorMessage;
+
 public class RacingCarGameBoardTest {
+
+    @Test
+    public void 옳은_입력() {
+        // given
+        List<RacingCar> carList = new ArrayList<>(
+                Arrays.asList(
+                        RacingCar.of("재롱"),
+                        RacingCar.of("동주"),
+                        RacingCar.of("우댕"),
+                        RacingCar.of("만선")));
+
+        // when, then
+        RacingCarGameBoard.of(carList);
+    }
+
+    @Test
+    public void 자동차_수가_많은_입력() {
+        // given
+        List<RacingCar> carList = new ArrayList<>();
+        int max = 100;
+        for (int i = 0; i < max + 1; i++) {
+            carList.add(RacingCar.of("자동차"));
+        }
+        // when, then
+        assertThatThrownBy(() -> RacingCarGameBoard.of(carList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.OUTRAGE_RACING_CAR_COUNT.getMessage(max));
+    }
+
+    @Test
+    public void 빈_입력() {
+        // given
+        List<RacingCar> carList = new ArrayList<>();
+        int max = 100;
+
+        // when, then
+        assertThatThrownBy(() -> RacingCarGameBoard.of(carList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.OUTRAGE_RACING_CAR_COUNT.getMessage(max));
+    }
+
+    @Test
+    public void 보드판에_기록_하기() {
+        // given
+        List<RacingCar> carList = new ArrayList<>(
+                Arrays.asList(
+                        RacingCar.of("재롱"),
+                        RacingCar.of("동주"),
+                        RacingCar.of("우댕"),
+                        RacingCar.of("만선")));
+
+        for (int i = 0; i < carList.size(); i++) {
+            for (int j = 0; j < i + 1; j++) {
+                carList.get(i).move();
+            }
+        }
+        RacingCarGameBoard racingCarGameBoard = RacingCarGameBoard.of(carList);
+        String expected = "재롱 : -\n동주 : --\n우댕 : ---\n만선 : ----\n\n";
+
+        // when
+        racingCarGameBoard.recordToBoard();
+
+        // then
+        assertThat(racingCarGameBoard.getRecordBoard()).isEqualTo(expected);
+
+    }
 }
--- src/test/java/racingcar/entity/RacingCarTest.java
+++ src/test/java/racingcar/entity/RacingCarTest.java
@@ -1,4 +1,41 @@
 package racingcar.entity;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import racingcar.error.ErrorMessage;
+
 public class RacingCarTest {
+    @Test
+    public void 옳은_입력() {
+        // given
+        String carName = "동주";
+
+        // when, then
+        RacingCar.of(carName);
+    }
+
+    @Test
+    public void 빈_입력() {
+        // given
+        String carName = "";
+        int maxValue = 5;
+
+        // when
+        assertThatThrownBy(() -> RacingCar.of(carName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_RACING_CAR_NAME.getMessage(maxValue));
+    }
+
+    @Test
+    public void 글자수가_큰_자동차_이름_입력() {
+        // given
+        String carName = "붕붕붕아주작은자동차꼬마자동차가나간다";
+        int maxValue = 5;
+
+        // when
+        assertThatThrownBy(() -> RacingCar.of(carName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_RACING_CAR_NAME.getMessage(maxValue));
+    }
 }
