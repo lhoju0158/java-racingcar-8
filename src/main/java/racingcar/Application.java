cea7960e25e3987aea7262f1d97732668322ca9c
lhoju0158 <lhoju0158@gmail.com>
2025-10-27 02:14:59 +0900

    feat(controller, Application): 프로그램 코드 구현 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import racingcar.controller.RacingCarGameController;
+import racingcar.service.racingcargame.CustomRacingCarGame;
+import racingcar.service.racingcargame.utils.CustomFormTranslator;
+import racingcar.service.racingcargame.utils.CustomMovingDecisionMaker;
+import racingcar.service.racingcargame.utils.CustomRandomNumberGenerator;
+import racingcar.view.CustomInputView;
+import racingcar.view.CustomOutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingCarGameController racingCarGameController
+                = RacingCarGameController.of(
+                CustomRacingCarGame.of(new CustomMovingDecisionMaker(new CustomRandomNumberGenerator()),
+                        new CustomFormTranslator()),
+                CustomInputView.of(), CustomOutputView.of());
+        racingCarGameController.run();
     }
 }
--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -1,4 +1,30 @@
 package racingcar.controller;
 
-public class RacingCarGame {
+
+import racingcar.dto.RacingCarGameInfo;
+import racingcar.dto.RacingCarGameResult;
+import racingcar.service.racingcargame.RacingCarGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingCarGameController {
+    private RacingCarGame racingCarGame;
+    private InputView inputView;
+    private OutputView outputView;
+
+    private RacingCarGameController(RacingCarGame racingCarGame, InputView inputView, OutputView outputView) {
+        this.racingCarGame = racingCarGame;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public static RacingCarGameController of(RacingCarGame racingCarGame, InputView inputView, OutputView outputView) {
+        return new RacingCarGameController(racingCarGame, inputView, outputView);
+    }
+
+    public void run() {
+        RacingCarGameInfo racingCarGameInfo = inputView.printGameInfoForm();
+        RacingCarGameResult racingCarGameResult = racingCarGame.play(racingCarGameInfo);
+        outputView.printGameResult(racingCarGameResult);
+    }
 }
